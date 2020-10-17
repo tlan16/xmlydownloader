83bd1c20b3ff84bf026060d60adb9a3ca493286a
Frank Lan <flan@msts.com>
2020-10-17 11:23:08 +1100

    add test assertions

--- core_test.go
+++ core_test.go
@@ -1,6 +1,7 @@
 package xmlydownloader
 
 import (
+	"github.com/stretchr/testify/assert"
 	"log"
 	"testing"
 )
@@ -12,6 +13,23 @@ func TestGetAlbumInfo(t *testing.T) {
 		t.Fatal(err)
 	}
 	t.Logf("%+v", ai)
+	assert.NotNil(t, ai)
+	assert.Equal(t, "0", ai.Msg, "Expect Msg to be string 0")
+	assert.Zero(t, ai.Ret, "Expect Reg to be number 0")
+	assert.Equal(t, true, ai.CostTimeAlbumInfo > 0, "Expect CostTimeAlbumInfo to be a positive number")
+	assert.Zero(t, ai.Ret, "Expect Reg to be number 0")
+	// assert album
+	assert.True(t, ai.Data.Album.AlbumID > 1, "Expect a valid Data.Album.AlbumID")
+	assert.True(t, len(ai.Data.Album.Title) > 0, "Expect Data.Album.Title to be an non-empty string")
+	assert.True(t, ai.Data.Album.IsPaid, "Expect paid album to be detected correctly indicated via Data.Album.IsPaid")
+	assert.True(t, ai.Data.Album.TrackCount > 0, "Expect Data.Album.TrackCount to be a positive number")
+	assert.Zero(t, ai.Data.Album.VipFreeType, "Expect vip free type to be detected correctly indicated via Data.Album.vipFreeType")
+	assert.True(t, len(ai.Data.Album.PriceTypes) > 0, "Expect Data.Album.PriceTypes to be an non-empty array")
+	for _, PriceType := range ai.Data.Album.PriceTypes {
+		assert.True(t, len(PriceType.FreeTrackIds) > 0, "Expect Data.Album.PriceTypes.FreeTrackIds to be an non-empty string")
+		assert.True(t, PriceType.FreeTrackCount > 0, "Expect Data.Album.PriceTypes.FreeTrackCount to be an positive number")
+	}
+	assert.Zero(t, ai.Data.Album.IsFinished, "Expect is album finished to be detected correctly indicated via Data.Album.IsFinished")
 }
 
 func TestGetAllTrackList(t *testing.T) {
@@ -21,6 +39,9 @@ func TestGetAllTrackList(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+	assert.NotNil(t, tracks)
+	assert.Equal(t, "0", tracks.Msg, "Expect Msg to be string 0")
+	assert.Zero(t, tracks.Ret, "Expect Reg to be number 0")
 	for _, v := range tracks.Data.List {
 		trackInfoList = append(trackInfoList, v)
 	}
@@ -37,6 +58,51 @@ func TestGetAllTrackList(t *testing.T) {
 	}
 	for i, v := range trackInfoList {
 		t.Log(i, v.Title)
+		assert.True(t, v.TrackID > 0)
+		assert.True(t, v.TrackRecordID > 0)
+		assert.True(t, v.UID > 0)
+		assert.Equal(t, v.IsFree, len(v.PlayURL64) > 0)
+		assert.Equal(t, v.IsFree, len(v.PlayURL32) > 0)
+		assert.Equal(t, v.IsFree, len(v.PlayPathAacv164) > 0)
+		assert.Equal(t, v.IsFree, len(v.PlayPathAacv224) > 0)
+		assert.True(t, v.Duration > 0)
+		assert.True(t, v.AlbumID > 0)
+		assert.True(t, v.IsPaid)
+		assert.NotNil(t, v.IsFree)
+		assert.False(t, v.IsVideo)
+		assert.False(t, v.IsDraft)
+		assert.False(t, v.IsRichAudio)
+		assert.False(t, v.IsAuthorized)
+		assert.True(t, v.Price > 0)
+		assert.True(t, v.DiscountedPrice > 0)
+		assert.True(t, v.PriceTypeID > 0)
+		assert.True(t, v.SampleDuration > 0)
+		assert.True(t, v.PriceTypeID > 0)
+		assert.True(t, len(v.DisplayPrice) > 0)
+		assert.True(t, len(v.DisplayDiscountedPrice) > 0)
+		assert.True(t, v.Type >= 0)
+		assert.True(t, v.RelatedID >= 0)
+		assert.True(t, v.OrderNo > 0)
+		assert.True(t, v.IsHoldCopyright)
+		assert.Equal(t, 0, v.VipFirstStatus)
+		assert.Equal(t, 0, v.PaidType)
+		assert.False(t, v.IsSample)
+		assert.True(t, v.ProcessState > 0)
+		assert.True(t, v.CreatedAt > 0)
+		assert.True(t, len(v.CoverSmall) > 0)
+		assert.True(t, len(v.CoverMiddle) > 0)
+		assert.True(t, len(v.CoverLarge) > 0)
+		assert.True(t, len(v.Nickname) > 0)
+		assert.True(t, len(v.SmallLogo) > 0)
+		assert.True(t, v.UserSource > 0)
+		assert.True(t, v.OpType > 0)
+		assert.True(t, v.IsPublic)
+		assert.True(t, v.Likes > 0)
+		assert.True(t, v.Playtimes >= 0)
+		assert.True(t, v.Comments >= 0)
+		assert.True(t, v.Shares >= 0)
+		assert.True(t, v.Status >= 0)
+		assert.False(t, v.IsTrailer)
 	}
 }
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/jing332/xmlydownloader
 
 go 1.14
 
-require github.com/json-iterator/go v1.1.10
+require (
+	github.com/json-iterator/go v1.1.10
+	github.com/stretchr/testify v1.3.0
+)
